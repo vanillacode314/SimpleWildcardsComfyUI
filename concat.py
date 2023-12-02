c27ca5eb2c6e0dba6b096eb91ec19901daddf172
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2023-12-03 01:31:05 +0530

    forceInput for concat widget

--- concat.py
+++ concat.py
@@ -12,71 +12,71 @@ class SimpleConcat:
                 "input_0": (
                     "STRING",
                     {
+                        "forceInput": True,
                         "default": "",
-                        "multiline": True,
                     },
                 ),
                 "input_1": (
                     "STRING",
                     {
+                        "forceInput": True,
                         "default": "",
-                        "multiline": True,
                     },
                 ),
                 "input_2": (
                     "STRING",
                     {
+                        "forceInput": True,
                         "default": "",
-                        "multiline": True,
                     },
                 ),
                 "input_3": (
                     "STRING",
                     {
+                        "forceInput": True,
                         "default": "",
-                        "multiline": True,
                     },
                 ),
                 "input_4": (
                     "STRING",
                     {
+                        "forceInput": True,
                         "default": "",
-                        "multiline": True,
                     },
                 ),
                 "input_5": (
                     "STRING",
                     {
+                        "forceInput": True,
                         "default": "",
-                        "multiline": True,
                     },
                 ),
                 "input_6": (
                     "STRING",
                     {
+                        "forceInput": True,
                         "default": "",
-                        "multiline": True,
                     },
                 ),
                 "input_7": (
                     "STRING",
                     {
+                        "forceInput": True,
                         "default": "",
-                        "multiline": True,
                     },
                 ),
                 "input_8": (
                     "STRING",
                     {
+                        "forceInput": True,
                         "default": "",
-                        "multiline": True,
                     },
                 ),
                 "input_9": (
                     "STRING",
                     {
+                        "forceInput": True,
                         "default": "",
-                        "multiline": True,
                     },
                 ),
                 "output_text": (
